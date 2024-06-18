e35a608d274e3b1e3e599b202f4ac6f48d657757
prnvvv <prannavakhanth12@gmail.com>
2024-06-18 21:49:02 +0530

    Update DateTimeDay.py

--- DateTimeDay.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-presentDay = datetime.datetime.now().strftime("%A")
-dateTime = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") 
-
-print()
-print(f"Today is {presentDay}")
-print(dateTime, end = "\n")
